15b7e7c63c928018cede71b811d17eb36afc2b80
allen <378917466@qq.com>
2018-11-02 14:54:54 +0800

    添加检测window服务是否存在验证按钮

--- WindowsServiceTest/WindowsServiceTest/ServiceTest.cs
+++ WindowsServiceTest/WindowsServiceTest/ServiceTest.cs
@@ -9,27 +9,27 @@ using System.Text;
 
 namespace WindowsServiceTest
 {
-	public partial class ServiceTest : ServiceBase
-	{
-		public ServiceTest()
-		{
-			InitializeComponent();
-		}
+    public partial class ServiceTest : ServiceBase
+    {
+        public ServiceTest()
+        {
+            InitializeComponent();
+        }
 
-		protected override void OnStart(string[] args)
-		{
-			using (System.IO.StreamWriter sw = new System.IO.StreamWriter("C:\\log.txt", true))
-			{
-				sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ") + "Start.");
-			}
-		}
+        protected override void OnStart(string[] args)
+        {
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("C:\\log.txt", true))
+            {
+                sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ") + "Start.");
+            }
+        }
 
-		protected override void OnStop()
-		{
-			using (System.IO.StreamWriter sw = new System.IO.StreamWriter("C:\\log.txt", true))
-			{
-				sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ") + "Stop.");
-			}
-		}
-	}
+        protected override void OnStop()
+        {
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("C:\\log.txt", true))
+            {
+                sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ") + "Stop.");
+            }
+        }
+    }
 }
--- WindowsServiceTest/WindowsServiceTestUI/MainWindow.xaml.cs
+++ WindowsServiceTest/WindowsServiceTestUI/MainWindow.xaml.cs
@@ -16,89 +16,120 @@ using System.ServiceProcess;
 
 namespace WindowsServiceTestUI
 {
-	/// <summary>
-	/// MainWindow.xaml 的交互逻辑
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
-		private void btnInstall_Click(object sender, RoutedEventArgs e)
-		{
-			string CurrentDirectory = System.Environment.CurrentDirectory;
-			System.Environment.CurrentDirectory = CurrentDirectory + "\\Service";
-			Process process = new Process();
-			process.StartInfo.UseShellExecute = false;
-			process.StartInfo.FileName = "Install.bat";
-			process.StartInfo.CreateNoWindow = true;
-			process.Start();
-			lblLog.Text = "安装成功";
-			System.Environment.CurrentDirectory = CurrentDirectory;
-		}
+        private void btnInstall_Click(object sender, RoutedEventArgs e)
+        {
+            string CurrentDirectory = System.Environment.CurrentDirectory;
+            System.Environment.CurrentDirectory = CurrentDirectory + "\\Service";
+            Process process = new Process();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.FileName = "Install.bat";
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            lblLog.Text = "安装成功";
+            System.Environment.CurrentDirectory = CurrentDirectory;
+        }
 
-		private void btnUninstall_Click(object sender, RoutedEventArgs e)
-		{
-			string CurrentDirectory = System.Environment.CurrentDirectory;
-			System.Environment.CurrentDirectory = CurrentDirectory + "\\Service";
-			Process process = new Process();
-			process.StartInfo.UseShellExecute = false;
-			process.StartInfo.FileName = "Uninstall.bat";
-			process.StartInfo.CreateNoWindow = true;
-			process.Start();
-			lblLog.Text = "卸载成功";
-			System.Environment.CurrentDirectory = CurrentDirectory;
-		}
+        private void btnUninstall_Click(object sender, RoutedEventArgs e)
+        {
+            string CurrentDirectory = System.Environment.CurrentDirectory;
+            System.Environment.CurrentDirectory = CurrentDirectory + "\\Service";
+            Process process = new Process();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.FileName = "Uninstall.bat";
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            lblLog.Text = "卸载成功";
+            System.Environment.CurrentDirectory = CurrentDirectory;
+        }
+        private bool IsServiceExisted(string serviceName)
+        {
+            ServiceController[] services = ServiceController.GetServices();
+            foreach (ServiceController s in services)
+            {
+                if (s.ServiceName == serviceName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
-		private void btnCheckStatus_Click(object sender, RoutedEventArgs e)
-		{
-			ServiceController serviceController = new ServiceController("ServiceTest");
-			lblCheckStatus.Text = serviceController.Status.ToString();
-		}
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (IsServiceExisted("ServiceTest"))
+            {
+                lblCheckStatus.Text = "服务已安装";
+            }
+            else
+            {
+                lblCheckStatus.Text = "服务未安装";
+            }
 
-		private void btnStart_Click(object sender, RoutedEventArgs e)
-		{
-			ServiceController serviceController = new ServiceController("ServiceTest");
-			serviceController.Start();
-			lblStatus.Text = "服务已启动";
-		}
+        }
+        private void btnCheckStatus_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ServiceController serviceController = new ServiceController("ServiceTest");
+                lblCheckStatus.Text = serviceController.Status.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
 
-		private void btnStop_Click(object sender, RoutedEventArgs e)
-		{
-			ServiceController serviceController = new ServiceController("ServiceTest");
-			if (serviceController.CanStop)
-			{
-				serviceController.Stop();
-				lblStatus.Text = "服务已停止";
-			}
-			else
-				lblStatus.Text = "服务不能停止";
-		}
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceController serviceController = new ServiceController("ServiceTest");
+            serviceController.Start();
+            lblStatus.Text = "服务已启动";
+        }
 
-		private void btnPauseContinue_Click(object sender, RoutedEventArgs e)
-		{
-			ServiceController serviceController = new ServiceController("ServiceTest");
-			if (serviceController.CanPauseAndContinue)
-			{
-				if (serviceController.Status == ServiceControllerStatus.Running)
-				{
-					serviceController.Pause();
-					lblStatus.Text = "服务已暂停";
-				}
-				else if (serviceController.Status == ServiceControllerStatus.Paused)
-				{
-					serviceController.Continue();
-					lblStatus.Text = "服务已继续";
-				}
-				else
-				{
-					lblStatus.Text = "服务未处于暂停和启动状态";
-				}
-			}
-			else
-				lblStatus.Text = "服务不能暂停";
-		}
-	}
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceController serviceController = new ServiceController("ServiceTest");
+            if (serviceController.CanStop)
+            {
+                serviceController.Stop();
+                lblStatus.Text = "服务已停止";
+            }
+            else
+                lblStatus.Text = "服务不能停止";
+        }
+
+        private void btnPauseContinue_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceController serviceController = new ServiceController("ServiceTest");
+            if (serviceController.CanPauseAndContinue)
+            {
+                if (serviceController.Status == ServiceControllerStatus.Running)
+                {
+                    serviceController.Pause();
+                    lblStatus.Text = "服务已暂停";
+                }
+                else if (serviceController.Status == ServiceControllerStatus.Paused)
+                {
+                    serviceController.Continue();
+                    lblStatus.Text = "服务已继续";
+                }
+                else
+                {
+                    lblStatus.Text = "服务未处于暂停和启动状态";
+                }
+            }
+            else
+                lblStatus.Text = "服务不能暂停";
+        }
+    }
 }
